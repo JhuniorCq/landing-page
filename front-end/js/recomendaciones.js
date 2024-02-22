1ce78ff5a6a1319db420a901ac655216b1d309ba
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-22 00:48:08 -0500

    .

--- front-end/js/recomendaciones.js
+++ front-end/js/recomendaciones.js
@@ -21,7 +21,7 @@ const agregarProductos = () => {
         const divProducto = document.createElement('div');
         divProducto.classList.add('contenedor-producto');
         divProducto.innerHTML = `
-            <img src="${objetoImagen['direccionImagen']}" alt="producto" class="producto">
+            <img src="./assets/img/instrumentos/${objetoImagen['direccionImagen']}" alt="producto" class="producto">
             <h3 class="titulo-producto">${objetoImagen['nombreInstrumento']}</h3>
             <p class="precio-producto">${objetoImagen['precioInstrumento']}</p>
             <button class="btn-escoger">Ver producto</button>
@@ -31,14 +31,14 @@ const agregarProductos = () => {
     
 }
 
-crearObjetoImagen('Guitarra Acústica – Ortega Student Series RST5M-3/4BK – Negro', `${pathImagenes}producto1.png`, 'S/. 519');
-crearObjetoImagen('Bajo – TAKEY – TK-JA5-LPB – Turqueza', `${pathImagenes}producto2.png`, 'S/. 949');
-crearObjetoImagen('Bajo – TAKEY – BT05-HC – Natural', `${pathImagenes}producto3.png`, 'S/. 2619');
-crearObjetoImagen('Piano Digital CASIO – Celviano AP-270BKC2 – Negro', `${pathImagenes}producto4.png`, 'S/. 1470');
-crearObjetoImagen('Cabezal para Guitarra – MARSHALL – JVM1H 50th – Negro', `${pathImagenes}producto5.png`, 'S/. 2919');
-crearObjetoImagen('Pedal Analogico KORG – NUVIBE', `${pathImagenes}producto6.png`, 'S/. 1020');
-crearObjetoImagen('Guitarra Elétrica – GIBSON – LPS500TONH1 – Tobacco Burst', `${pathImagenes}producto7.png`, 'S/. 919');
-crearObjetoImagen('Medeli YODRUM – Batería electrónica digital', `${pathImagenes}producto8.png`, 'S/. 4700');
-crearObjetoImagen('Ukelele Concierto Flight DUC380 Jade', `${pathImagenes}producto9.png`, 'S/. 230');
+crearObjetoImagen('Guitarra Acústica – Ortega Student Series RST5M-3/4BK – Negro', 'producto1.png', 'S/. 519');
+crearObjetoImagen('Bajo – TAKEY – TK-JA5-LPB – Turqueza', 'producto2.png', 'S/. 949');
+crearObjetoImagen('Bajo – TAKEY – BT05-HC – Natural', 'producto3.png', 'S/. 2619');
+crearObjetoImagen('Piano Digital CASIO – Celviano AP-270BKC2 – Negro', 'producto4.png', 'S/. 1470');
+crearObjetoImagen('Cabezal para Guitarra – MARSHALL – JVM1H 50th – Negro', 'producto5.png', 'S/. 2919');
+crearObjetoImagen('Pedal Analogico KORG – NUVIBE', 'producto6.png', 'S/. 1020');
+crearObjetoImagen('Guitarra Elétrica – GIBSON – LPS500TONH1 – Tobacco Burst', 'producto7.png', 'S/. 919');
+crearObjetoImagen('Medeli YODRUM – Batería electrónica digital', 'producto8.png', 'S/. 4700');
+crearObjetoImagen('Ukelele Concierto Flight DUC380 Jade', 'producto9.png', 'S/. 230');
 
 agregarProductos();
