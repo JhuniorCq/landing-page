1831a42b5b49166f2e0739f3566829e926692b2a
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-21 22:17:31 -0500

    .

--- front-end/js/recomendaciones.js
+++ front-end/js/recomendaciones.js
@@ -30,14 +30,14 @@ const agregarProductos = () => {
     
 }
 
-crearObjetoImagen('Guitarra Acústica – Ortega Student Series RST5M-3/4BK – Negro', `${pathImagenes}1.png`, 'S/. 519');
-crearObjetoImagen('Bajo – TAKEY – TK-JA5-LPB – Turqueza', `${pathImagenes}2.png`, 'S/. 949');
-crearObjetoImagen('Bajo – TAKEY – BT05-HC – Natural', `${pathImagenes}3.png`, 'S/. 2619');
-crearObjetoImagen('Piano Digital CASIO – Celviano AP-270BKC2 – Negro', `${pathImagenes}4.png`, 'S/. 1470');
-crearObjetoImagen('Cabezal para Guitarra – MARSHALL – JVM1H 50th – Negro', `${pathImagenes}5.png`, 'S/. 2919');
-crearObjetoImagen('Pedal Analogico KORG – NUVIBE', `${pathImagenes}6.png`, 'S/. 1020');
-crearObjetoImagen('Guitarra Elétrica – GIBSON – LPS500TONH1 – Tobacco Burst', `${pathImagenes}7.png`, 'S/. 919');
-crearObjetoImagen('Medeli YODRUM – Batería electrónica digital', `${pathImagenes}8.png`, 'S/. 4700');
-crearObjetoImagen('Ukelele Concierto Flight DUC380 Jade', `${pathImagenes}9.png`, 'S/. 230');
+crearObjetoImagen('Guitarra Acústica – Ortega Student Series RST5M-3/4BK – Negro', `${pathImagenes}producto1.png`, 'S/. 519');
+crearObjetoImagen('Bajo – TAKEY – TK-JA5-LPB – Turqueza', `${pathImagenes}producto12.png`, 'S/. 949');
+crearObjetoImagen('Bajo – TAKEY – BT05-HC – Natural', `${pathImagenes}producto13.png`, 'S/. 2619');
+crearObjetoImagen('Piano Digital CASIO – Celviano AP-270BKC2 – Negro', `${pathImagenes}producto14.png`, 'S/. 1470');
+crearObjetoImagen('Cabezal para Guitarra – MARSHALL – JVM1H 50th – Negro', `${pathImagenes}producto15.png`, 'S/. 2919');
+crearObjetoImagen('Pedal Analogico KORG – NUVIBE', `${pathImagenes}producto16.png`, 'S/. 1020');
+crearObjetoImagen('Guitarra Elétrica – GIBSON – LPS500TONH1 – Tobacco Burst', `${pathImagenes}producto17.png`, 'S/. 919');
+crearObjetoImagen('Medeli YODRUM – Batería electrónica digital', `${pathImagenes}producto18.png`, 'S/. 4700');
+crearObjetoImagen('Ukelele Concierto Flight DUC380 Jade', `${pathImagenes}producto19.png`, 'S/. 230');
 
 agregarProductos();
