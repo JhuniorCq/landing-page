f0f744867b7ec3ed6304435d1c35d5a1204ce23c
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-21 23:40:06 -0500

    .

--- front-end/js/recomendaciones.js
+++ front-end/js/recomendaciones.js
@@ -1,6 +1,6 @@
 
 const contenedorProductos = document.querySelector('.contenedor-productos');
-const pathImagenes = '../../front-end/assets/img/';
+const pathImagenes = '../../front-end/assets/img/instrumentos/';
 const arrayObjetosImagenes = [];
 console.log(contenedorProductos)
 
@@ -31,13 +31,13 @@ const agregarProductos = () => {
 }
 
 crearObjetoImagen('Guitarra Acústica – Ortega Student Series RST5M-3/4BK – Negro', `${pathImagenes}producto1.png`, 'S/. 519');
-crearObjetoImagen('Bajo – TAKEY – TK-JA5-LPB – Turqueza', `${pathImagenes}producto12.png`, 'S/. 949');
-crearObjetoImagen('Bajo – TAKEY – BT05-HC – Natural', `${pathImagenes}producto13.png`, 'S/. 2619');
-crearObjetoImagen('Piano Digital CASIO – Celviano AP-270BKC2 – Negro', `${pathImagenes}producto14.png`, 'S/. 1470');
-crearObjetoImagen('Cabezal para Guitarra – MARSHALL – JVM1H 50th – Negro', `${pathImagenes}producto15.png`, 'S/. 2919');
-crearObjetoImagen('Pedal Analogico KORG – NUVIBE', `${pathImagenes}producto16.png`, 'S/. 1020');
-crearObjetoImagen('Guitarra Elétrica – GIBSON – LPS500TONH1 – Tobacco Burst', `${pathImagenes}producto17.png`, 'S/. 919');
-crearObjetoImagen('Medeli YODRUM – Batería electrónica digital', `${pathImagenes}producto18.png`, 'S/. 4700');
-crearObjetoImagen('Ukelele Concierto Flight DUC380 Jade', `${pathImagenes}producto19.png`, 'S/. 230');
+crearObjetoImagen('Bajo – TAKEY – TK-JA5-LPB – Turqueza', `${pathImagenes}producto2.png`, 'S/. 949');
+crearObjetoImagen('Bajo – TAKEY – BT05-HC – Natural', `${pathImagenes}producto3.png`, 'S/. 2619');
+crearObjetoImagen('Piano Digital CASIO – Celviano AP-270BKC2 – Negro', `${pathImagenes}producto4.png`, 'S/. 1470');
+crearObjetoImagen('Cabezal para Guitarra – MARSHALL – JVM1H 50th – Negro', `${pathImagenes}producto5.png`, 'S/. 2919');
+crearObjetoImagen('Pedal Analogico KORG – NUVIBE', `${pathImagenes}producto6.png`, 'S/. 1020');
+crearObjetoImagen('Guitarra Elétrica – GIBSON – LPS500TONH1 – Tobacco Burst', `${pathImagenes}producto7.png`, 'S/. 919');
+crearObjetoImagen('Medeli YODRUM – Batería electrónica digital', `${pathImagenes}producto8.png`, 'S/. 4700');
+crearObjetoImagen('Ukelele Concierto Flight DUC380 Jade', `${pathImagenes}producto9.png`, 'S/. 230');
 
 agregarProductos();
